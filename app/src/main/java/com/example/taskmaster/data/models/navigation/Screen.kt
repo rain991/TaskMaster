37c069e4425a6fd7a94aa081f702c2c3d393b576
Savenko Ivan PC <savenkoivan931@gmail.com>
2024-03-29 13:52:52 +0100

    TaskMasterBottomBar.kt

--- app/src/main/java/com/example/taskmaster/data/models/navigation/Screen.kt
+++ app/src/main/java/com/example/taskmaster/data/models/navigation/Screen.kt
@@ -5,7 +5,6 @@ sealed class Screen(val route: String) {
     object TaskScreen : Screen("task_screen")
     object FinishedScreen : Screen("finished_screen")
     object GroupsScreen : Screen("group_screen")
-
     // teacher related
     object CreateTaskScreen : Screen("create_task_screen")
 }
